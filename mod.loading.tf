84c2215cb1538a5601c4333da9dd9412104da919
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-15 17:26:39 +0400

    Initial commit

--- mod.loading.tf
+++ mod.loading.tf
@@ -1,13 +1,13 @@
 /*Data loading...*/
 locals {
-  data-key-pairs       = try(jsondecode(file(var.file-key-pairs)), [])
-  data-nework-acls     = try(jsondecode(file(var.file-nework-acls)), [])
-  data-security-groups = try(jsondecode(file(var.file-security-groups)), [])
+  data-key-pairs   = try(jsondecode(file(var.file-key-pairs)), [])
+  data-nework-acls = try(jsondecode(file(var.file-nework-acls)), [])
+  //data-security-groups = try(jsondecode(file(var.file-security-groups)), [])
 }
 
 /*Data decoding...*/
 locals {
-  lst-key-pairs       = try({ for obj in local.data-key-pairs.key-pairs : obj.name => obj }, tomap({}))
-  lst-nework-acls     = try({ for obj in local.data-nework-acls.nework-acls : obj.name => obj }, tomap({}))
-  lst-security-groups = try({ for obj in local.data-security-groups.security-groups : obj.name => obj }, tomap({}))
+  lst-key-pairs   = try({ for obj in local.data-key-pairs.key-pairs : obj.name => obj }, tomap({}))
+  lst-nework-acls = try({ for obj in local.data-nework-acls.nework-acls : obj.name => obj }, tomap({}))
+  //lst-security-groups = try({ for obj in local.data-security-groups.security-groups : obj.name => obj }, tomap({}))
 }
