5f2792b54eb65a2daa19f0e7ec51780931500de0
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-15 17:33:47 +0400

    Initial commit

--- mod.loading.tf
+++ mod.loading.tf
@@ -1,13 +1,13 @@
 /*Data loading...*/
 locals {
-  data-key-pairs   = try(jsondecode(file(var.file-key-pairs)), [])
-  data-nework-acls = try(jsondecode(file(var.file-nework-acls)), [])
+  data-key-pairs    = try(jsondecode(file(var.file-key-pairs)), [])
+  data-network-acls = try(jsondecode(file(var.file-network-acls)), [])
   //data-security-groups = try(jsondecode(file(var.file-security-groups)), [])
 }
 
 /*Data decoding...*/
 locals {
-  lst-key-pairs   = try({ for obj in local.data-key-pairs.key-pairs : obj.name => obj }, tomap({}))
-  lst-nework-acls = try({ for obj in local.data-nework-acls.nework-acls : obj.name => obj }, tomap({}))
+  lst-key-pairs    = try({ for obj in local.data-key-pairs.key-pairs : obj.name => obj }, tomap({}))
+  lst-network-acls = try({ for obj in local.data-network-acls.network-acls : obj.name => obj }, tomap({}))
   //lst-security-groups = try({ for obj in local.data-security-groups.security-groups : obj.name => obj }, tomap({}))
 }
