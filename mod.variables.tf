383956351d53eca99d4c07010e91e1e94ee11b31
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-20 18:31:29 +0400

    Initial commit

--- mod.variables.tf
+++ mod.variables.tf
@@ -1,6 +1,6 @@
 /*Initialization...*/
-variable "data-vpcs" {}
-variable "data-vpc-subnets" {}
+variable "data-vpcs" { default = [] }
+variable "data-vpc-subnets" { default = [] }
 variable "file-key-pairs" { default = "" }
 variable "file-network-acls" { default = "" }
 variable "file-security-groups" { default = "" }
